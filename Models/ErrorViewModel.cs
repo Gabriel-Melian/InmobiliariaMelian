91fab4eeb5e523191b962fbbd4373db0b2b83cd3
Gabriel Melian <thegaboosx@gmail.com>
2025-08-22 16:13:32 -0300

    Primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InmobiliariaMelian.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
